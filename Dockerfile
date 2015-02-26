bf419c0e361a4c54c1c1262b2a2e4991f32e76a3
Michael Timbrook <timbrook480@gmail.com>
2015-02-26 15:35:41 -0500

    Pulls zk config from consul

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ RUN apt-get update; apt-get install -y unzip  openjdk-6-jdk wget git docker.io
 RUN wget -q http://apache.mirrors.lucidnetworks.net/kafka/0.8.2.0/kafka_2.10-0.8.2.0.tgz -O /tmp/kafka_2.10-0.8.2.0.tgz
 RUN tar xfz /tmp/kafka_2.10-0.8.2.0.tgz -C /opt
 
+RUN apt-get install -y curl
+
 VOLUME ["/kafka"]
 
 ADD ./config /opt/kafka_2.10-0.8.2.0/config
@@ -14,4 +16,6 @@ ADD ./config /opt/kafka_2.10-0.8.2.0/config
 ENV KAFKA_HOME /opt/kafka_2.10-0.8.2.0
 ADD start-kafka.sh /usr/bin/start-kafka.sh
 ADD broker-list.sh /usr/bin/broker-list.sh
+ADD test.sh /usr/bin/test.sh
+		
 CMD start-kafka.sh
--- start-kafka.sh
+++ start-kafka.sh
@@ -10,7 +10,22 @@ if [[ -z "$KAFKA_LOG_DIRS" ]]; then
     export KAFKA_LOG_DIRS="/kafka/kafka-logs-$KAFKA_BROKER_ID"
 fi
 if [[ -z "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
-    export KAFKA_ZOOKEEPER_CONNECT=$(env | grep ZK.*PORT_2181_TCP= | sed -e 's|.*tcp://||' | paste -sd ,)
+    function jsonval {
+        temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop| cut -d":" -f2| sed -e 's/^ *//g' -e 's/ *$//g'`
+        echo ${temp##*|}
+    }
+     
+    json=`curl -s -X GET $CONSUL_URL/v1/catalog/service/zookeeper-2181`
+    prop='ServicePort'
+    port=`jsonval`
+
+    json=`curl -s -X GET $CONSUL_URL/v1/catalog/service/zookeeper-2181`
+    prop='Address'
+    address=`jsonval`
+
+    export KAFKA_ZOOKEEPER_CONNECT=$(echo ${address}:${port%?})
+
+    echo $KAFKA_ZOOKEEPER_CONNECT
 fi
 
 if [[ -n "$KAFKA_HEAP_OPTS" ]]; then
