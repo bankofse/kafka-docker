e37137e530e8ede224c9cade090046434942684c
Michael Timbrook <timbrook480@gmail.com>
2015-03-02 18:42:00 -0500

    Updated build with dnsutils

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:trusty
 
 MAINTAINER Wurstmeister 
 
-RUN apt-get update; apt-get install -y unzip  openjdk-6-jdk wget git docker.io
+RUN apt-get update; apt-get install -y unzip dnsutils openjdk-6-jdk wget git docker.io
 
 RUN wget -q http://mirror.cogentco.com/pub/apache/kafka/0.8.2.0/kafka_2.10-0.8.2.0.tgz -O /tmp/kafka_2.10-0.8.2.0.tgz
 RUN tar xfz /tmp/kafka_2.10-0.8.2.0.tgz -C /opt
--- start-kafka.sh
+++ start-kafka.sh
@@ -10,25 +10,13 @@ if [[ -z "$KAFKA_LOG_DIRS" ]]; then
     export KAFKA_LOG_DIRS="/kafka/kafka-logs-$KAFKA_BROKER_ID"
 fi
 if [[ -z "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
-    function jsonval {
-        temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop| cut -d":" -f2| sed -e 's/^ *//g' -e 's/ *$//g'`
-        echo ${temp##*|}
-    }
-     
-    json=`curl -s -X GET $CONSUL_URL/v1/catalog/service/zookeeper-2181`
-    prop='ServicePort'
-    port=`jsonval`
 
-    json=`curl -s -X GET $CONSUL_URL/v1/catalog/service/zookeeper-2181`
-    prop='Address'
-    address=`jsonval`
+    address=`dig @$CONSUL_URL zookeeper-2181.service.nyc.consul +short`
+    port=`dig @$CONSUL_URL zookeeper-2181.service.nyc.consul SRV +short | awk '{print $3}'`
 
-    echo $address
-    echo ${port%?}
-
-    export KAFKA_ZOOKEEPER_CONNECT=$(echo ${address}:${port%?}) 
-    echo "Connect address"
+    export KAFKA_ZOOKEEPER_CONNECT=$(echo ${address}:${port}) 
     echo $KAFKA_ZOOKEEPER_CONNECT
+
 fi
 
 if [[ -n "$KAFKA_HEAP_OPTS" ]]; then
